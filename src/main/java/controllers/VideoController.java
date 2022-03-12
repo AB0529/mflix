dabe4f1fc93c8d5c57325765c0b09a18d1d375a2
AB0529 <supercrab115@gmail.com>
2022-03-11 19:38:10 -0500

    Initial commit

---
+++ src/main/java/controllers/VideoController.java
@@ -0,0 +1,9 @@
+package controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RestController("/api/video")
+public class VideoController {
+    @Auto
+}
